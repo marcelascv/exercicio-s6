fe9fac489087e0b9fd1e93038b48a95b74a6e97a
marcelascv <adm.marcelavasconcelos@gmail.com>
2020-09-19 23:55:48 -0300

    exercicio s6 4/10

---
+++ para-casa/cursosDeGraduacao.js
@@ -0,0 +1,52 @@
+[
+    {
+        "curso": "Administração",
+        "area": "Humanas",
+        "duracaoAno": "4"
+    },
+    {
+        "curso": "Psicologia",
+        "area": "Humanas",
+        "duracaoAno": "5"
+    },
+    {
+        "curso": "Medicina",
+        "area": "Saúde",
+        "duracaoAno": "5"
+    },
+    {
+        "curso": "Engenharia Civil",
+        "area": "Exatas",
+        "duracaoAno": "5"
+    },
+    {
+        "curso": "Ciência da Computação",
+        "area": "Exatas",
+        "duracaoAno": "4"
+    },
+    {
+        "curso": "Serviço Social",
+        "area": "Humanas",
+        "duracaoAno": "4"
+    },
+    {
+        "curso": "Ciências Contábeis",
+        "area": "Humanas",
+        "duracaoAno": "4"
+    },
+    {
+        "curso": "Design",
+        "area": "Humanas",
+        "duracaoAno": "4"
+    },
+    {
+        "curso": "Nutrição",
+        "area": "Saúde",
+        "duracaoAno": "4"
+    },
+    {
+        "curso": "Direito",
+        "area": "Humanas",
+        "duracaoAnos": "5"
+    }
+]
